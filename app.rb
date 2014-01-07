240382b4ebca85ebd9feea2998e7fd82e40db5d0
gf <g@g.fr>
2014-01-07 08:12:30 +0100

    add extensible conf file

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ ENV['RAILS_ENV'] = 'production'
 
 require './lib/db_connect'
 require './models/calendar'
-require 'active_support'
+require './models/action'
 
 require 'yaml'
 require 'haml'
@@ -17,8 +17,13 @@ class AxAgenda < Sinatra::Base
     haml :index
   end
 
+  # startDate=2013-12-29&endDate=2014-02-01&page=1&start=0&limit=25 
+  get '/events' do
+    # events = [:je, :jd].map do |srv| Action.server(srv).ax_find params end
+    '{}'
+  end
+  
   get '/calendars' do
-    calendars = []
     calendars = [:je, :jd].map do |srv| Calendar.server(srv).first.as_ax end
     { :calendars => calendars }.to_json
   end
---
+++ models/action.rb
@@ -0,0 +1,15 @@
+
+
+class Action < Sequel::Model
+  set_dataset :llx_actioncomm
+
+  class << self
+
+    def ax_find
+      
+    end
+
+  end
+
+
+end
