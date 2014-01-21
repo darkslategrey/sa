271cb6fc3b48068d882260f7917c35a68e3612ef
gf <g@g.fr>
2014-01-21 04:38:24 +0100

    change conf dayview to be more readable

--- public/js/app/view/CalendarPanel.js
+++ public/js/app/view/CalendarPanel.js
@@ -31,17 +31,26 @@ Ext.define('AxAgenda.view.CalendarPanel', {
     // 	'Extensible.calendar.CalendarPanel', 'AxAgenda.store.Events', 'AxAgenda.store.Calendars'
     // ],
 
-    activeItem: 3, // month view
+    activeItem: 2, // month view
     startDay: 0, // The 0-based index for the day on which the calendar week begins (0=Sunday)
     
     // Any generic view options that should be applied to all sub views:
     viewConfig: {
         //enableFx: false,
-        //ddIncrement: 10, //only applies to DayView and subclasses, but convenient to put it here
-        //viewStartHour: 6,
-        //viewEndHour: 18,
-        //minEventDisplayMinutes: 15
-        showTime: false
+        ddIncrement: 10, //only applies to DayView and subclasses, but convenient to put it here
+        viewStartHour: 8,
+        // viewEndHour: 18,
+        minEventDisplayMinutes: 5,
+        showTime: true
+    },
+
+    dayViewCfg: {
+        ddIncrement: 10, //only applies to DayView and subclasses, but convenient to put it here
+        // viewStartHour: 6,
+        // viewEndHour: 18,
+        minEventDisplayMinutes: 5,
+        showTime: true,
+	hourHeight: 100
     },
     
     // View options specific to a certain view (if the same options exist in viewConfig
@@ -51,8 +60,13 @@ Ext.define('AxAgenda.view.CalendarPanel', {
         showWeekLinks: true,
         showWeekNumbers: true
     },
+
+    weekViewCfg: {
+	hourHeight: 100	
+    },
     
     multiWeekViewCfg: {
+	hourHeight: 100
         //weekCount: 3
     },
 
