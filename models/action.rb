62f88b654418d9e849d213d05e5dd43aaa6d20e5
Grégory Faruch <greg.faruch@gmai.com>
2014-01-28 11:49:39 +0100

    clean

--- models/action.rb
+++ models/action.rb
@@ -9,7 +9,7 @@ class Action < Sequel::Model
   
   set_dataset :llx_actioncomm
 
-  set_allowed_columns :calendar, :datep, :datep2
+  set_allowed_columns :calendar, :datep, :datep2, :label
   
   many_to_one :calendar, :key => :ax_agenda_id
   many_to_one :type,     :key => :fk_action
@@ -24,19 +24,6 @@ class Action < Sequel::Model
   # => 86340 secondes dans 23h59 minutes
   # c'est de cette façon que le doli identifit les fullday
   DOLI_FULLDAY = 60 * 60 * 23 + (60 * 59)
-    # events = [ {
-    #              "id"    => 1001,
-    #              "cid"   => 1,
-    #              "title" => "Vacation",
-    #              "start" => "2014-01-07T10:00:00+01:00",
-    #              "end"   => "2014-01-07T11:00:00+01:00",
-    #              "notes" => "Have fun" }]
-
-    # params = {"startDate"=>"2013-12-29",
-    #           "endDate"=>"2014-02-01",
-    #           "page"=>"1",
-    #           "start"=>"0",
-    #           "limit"=>"25"}
   
   def self.ax_find params, srvs
     startDate = DateTime.parse params['startDate']
@@ -45,17 +32,20 @@ class Action < Sequel::Model
     # Attention ici sur le ^Société !! Risque de ne pas voir l'action
     labelcond = "label not regexp '^Bon de commande|^Facture FA|^Facture AV|^Proposition valid|^Société'"
     usercond  = "fk_user_action IS NOT NULL"
-    # labelcond = "label != 
     datecond  = "((datep >= '#{startDate}' and datep2 < '#{endDate}') OR (datep >= '#{startDate}' and datep2 IS NULL))"
     condition = "#{datecond} and #{usercond} and #{labelcond} and percent != 100"
     Action.logger.info "condition #{condition}"
 
     actions = []
     srvs.each do |srv|
-      actions += self.server(srv).where(condition).order(:datep, :datec).all
+      actions += self.server(srv).where(condition).order(:datep, :datec, :id).all
+      Action.logger.info "srv #{srv} nbr events #{actions.size}"
     end
     actions = reorder actions.flatten
-    actions = actions.map do |action| action.to_ax end
+    actions = actions.map do |action|
+      logger.info "found event #{action.id} / <#{action.label}>"
+      action.to_ax
+    end
   end    
 
   # Les actions doli toute la journée sont convertis en events AX
@@ -66,13 +56,8 @@ class Action < Sequel::Model
     actions.each do |action|
       datep, datep2  = action.datep, action.datep2
       d, m, y        = datep.day, datep.month, datep.year
-      sorted_actions = ordered_actions["#{d}-#{m}-#{y}"] || {:fullday => [], :rdv => []} 
-      if action.is_fullday # test le tout dans fullday 
-        sorted_actions[:fullday] << action
-      else
-        sorted_actions[:fullday] << action        
-        # sorted_actions[:rdv] << action
-      end
+      sorted_actions = ordered_actions["#{d}-#{m}-#{y}"] || {:fullday => [], :rdv => []} # rdv not used
+      sorted_actions[:fullday] << action
       ordered_actions["#{d}-#{m}-#{y}"] = sorted_actions
     end
 
@@ -88,26 +73,12 @@ class Action < Sequel::Model
         nd += interval.minute
         converted_actions << action
       end
-      # converted_actions += actions[:rdv]
     end
     converted_actions
   end
 
-    # EventId: "1088"
-    # all_day: false
-    # cid: "2"
-    # cname: "jd"
-    # end: "2014-01-14T10:30:00+01:00"
-    # id: "1088"
-    # location: ""
-    # notes: null
-    # owner: 2
-    # reminder: ""
-    # start: "2014-01-14T10:00:00+01:00"
-    # title: "1088 / RDV Client Synhèse et début collaboration jhjjkhkj"
-    # url: ""
   def self.update_doli params
-    self.logger.debug("Start update <#{params['cname']}> <#{params['id']}>")
+    self.logger.debug("Start update <#{params['cname']}> <#{params}>")
     msg = ''
     success = false
     action = Action.server(params['cname'].to_sym).where(:id => params['id']).first
@@ -117,11 +88,10 @@ class Action < Sequel::Model
     action.datep2 = params['end']
 
     if not action.contact.nil?
-      action.contact.phone = params['contact_phone']
-      action.contact.phone_perso = params['contact_phone_perso']
+      action.contact.phone        = params['contact_phone']
+      action.contact.phone_perso  = params['contact_phone_perso']
       action.contact.phone_mobile = params['contact_phone_mobile']
-      action.contact.email = params['contact_email']
-
+      action.contact.email        = params['contact_email']
       begin
         action.contact.save
       rescue Exception => e
@@ -153,8 +123,6 @@ class Action < Sequel::Model
   end
 
   def to_ax
-    environment = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
-    # title = environment == 'development' ? "#{id} / " : ''
     title = "#{id} / "
     title += societe.nil? ? label : societe.nom
     title += ' - '
@@ -163,14 +131,13 @@ class Action < Sequel::Model
     Action.logger.info "to ax pour #{id} calendar #{calendar}"
     Action.logger.info "calendar id #{calendar.id}"
 
-
     shortname = calendar.name =~ /Jobenfance/ ? 'je' : 'jd'
 
     contact_email = contact_phone = contact_phone_perso = contact_phone_mobile = ''
     if not contact.nil?
-      contact_email = contact.email
-      contact_phone = contact.phone
-      contact_phone_perso = contact.phone_perso
+      contact_email        = contact.email
+      contact_phone        = contact.phone
+      contact_phone_perso  = contact.phone_perso
       contact_phone_mobile = contact.phone_mobile
     end
 
